be0659114b8ab6e46f8e39590c19f452332666cf
nelvinProjects <nelvin.joseph@academytrainee.com>
2018-10-25 13:02:27 +0100

    Removed unwanted class

--- app/models/GetMovies.scala
+++ app/models/GetMovies.scala
@@ -9,13 +9,6 @@ object GetMovies {
     println(movieDetails("335983"))
   }
 
-  def nowRunning(): Unit = {
-    val html = Source.fromURL(
-      "https://api.themoviedb.org/3/movie/now_playing?api_key=487ca347f667dcb85f52d03ca278aded&language=en-UK&page=1")
-    val s: JsValue = Json.parse(html.mkString)
-    println(s \\ "title")
-  }
-
   def movieDetails(id: String): String = {
     val movieData = Json.parse(Source.fromURL(
       s"https://api.themoviedb.org/3/movie/$id?api_key=487ca347f667dcb85f52d03ca278aded&language=en-UK"
@@ -24,37 +17,16 @@ object GetMovies {
       s"https://api.themoviedb.org/3/movie/$id/videos?api_key=487ca347f667dcb85f52d03ca278aded&language=en-UK"
     ).mkString)
 
-
-    //    println(movieData)
-    println((movieData \\ "title") (0))
     val title = (movieData \\ "title").head
-    println(movieData \ "genres" \\ "name")
     val genre = (movieData \ "genres" \\ "name")
-    println(movieData \\ "overview")
     val overview = (movieData \\ "overview").head
-    println(movieData \\ "poster_path")
     val poster = (movieData \\ "poster_path").head
-    println(movieData \\ "release_date")
     val release = (movieData \\ "release_date").head
-    println(movieData \\ "runtime")
     val runtime = (movieData \\ "runtime").head
-
     val backdrop = (movieData \\ "backdrop_path").head
-    println(backdrop)
-
-    println((videoData \\ "key"))
     val video = (videoData \\ "key").head
-    println(video)
-
-    //    val map = Map("title" -> title, "genre" -> genre, "overview" -> overview,
-    //      "poster" -> poster, "release" -> release, "runtime" -> runtime, "video" -> video)
 
     var map = scala.collection.immutable.Seq(title, overview, poster, release, runtime, video, backdrop)
-    //    val map = (title, overview)
-    //    if (genre.length > 1) map = map :+ genre(1)
-    //    (genre.length) match {
-    //      case
-    //    }
     genre.foreach(x => map = map :+ x)
     Json.toJson(map).toString()
   }
