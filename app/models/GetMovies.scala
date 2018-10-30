7f7417f5e40d19df703aecad366ca4d5d5d19b8c
nelvinProjects <nelvin.joseph@academytrainee.com>
2018-10-30 15:56:40 +0000

    added tests for film details

--- app/models/GetMovies.scala
+++ app/models/GetMovies.scala
@@ -3,32 +3,37 @@ package models
 import play.api.libs.json.{JsValue, Json}
 import scala.io.Source
 
+/**
+  * Get movie details depending on the ID passed in
+  *
+  * @author: Nelvin
+  */
 object GetMovies {
-
-  def main(args: Array[String]): Unit = {
-    println(movieDetails("335983"))
-  }
-
   def movieDetails(id: String): String = {
-    val movieData = Json.parse(Source.fromURL(
-      s"https://api.themoviedb.org/3/movie/$id?api_key=487ca347f667dcb85f52d03ca278aded&language=en-UK"
-    ).mkString)
-    val videoData = Json.parse(Source.fromURL(
-      s"https://api.themoviedb.org/3/movie/$id/videos?api_key=487ca347f667dcb85f52d03ca278aded&language=en-UK"
-    ).mkString)
+    try {
+      val movieData = Json.parse(Source.fromURL(
+        s"https://api.themoviedb.org/3/movie/$id?api_key=487ca347f667dcb85f52d03ca278aded&language=en-UK"
+      ).mkString)
+      val videoData = Json.parse(Source.fromURL(
+        s"https://api.themoviedb.org/3/movie/$id/videos?api_key=487ca347f667dcb85f52d03ca278aded&language=en-UK"
+      ).mkString)
 
-    val title = (movieData \\ "title").head
-    val genre = (movieData \ "genres" \\ "name")
-    val overview = (movieData \\ "overview").head
-    val poster = (movieData \\ "poster_path").head
-    val release = (movieData \\ "release_date").head
-    val runtime = (movieData \\ "runtime").head
-    val backdrop = (movieData \\ "backdrop_path").head
-    val video = (videoData \\ "key").head
+      val title = (movieData \\ "title").head
+      val genre = (movieData \ "genres" \\ "name")
+      val overview = (movieData \\ "overview").head
+      val poster = (movieData \\ "poster_path").head
+      val release = (movieData \\ "release_date").head
+      val runtime = (movieData \\ "runtime").head
+      val backdrop = (movieData \\ "backdrop_path").head
+      val video = (videoData \\ "key").head
 
-    var map = scala.collection.immutable.Seq(title, overview, poster, release, runtime, video, backdrop)
-    genre.foreach(x => map = map :+ x)
-    Json.toJson(map).toString()
+      var map = scala.collection.immutable.Seq(title, overview, poster, release, runtime, video, backdrop)
+      genre.foreach(x => map = map :+ x)
+      Json.toJson(map).toString()
+    } catch {
+      case _: Throwable => Json.toJson("No Movie").toString()
+    }
   }
+
 
 }
--- build.sbt
+++ build.sbt
@@ -12,7 +12,8 @@ libraryDependencies ++= Seq(jdbc, cache, ws, specs2 % Test,
   "com.typesafe.slick" %% "slick" % "3.2.3",
   "org.slf4j" % "slf4j-nop" % "1.6.4",
   "com.typesafe.slick" %% "slick-hikaricp" % "3.2.3",
-  "mysql" % "mysql-connector-java" % "8.0.12")
+  "mysql" % "mysql-connector-java" % "8.0.12",
+  "org.scalatest" %% "scalatest" % "3.0.5" % Test)
 
 unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
 
