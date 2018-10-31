62340558ce54ae227bdeda39a2d86a997c23c971
nelvinProjects <nelvin.joseph@academytrainee.com>
2018-10-31 15:59:56 +0000

    adverts done, added nav/footer to movie details page, linked advert with movie detail page, nav bar button redirect added,css design for nav bar altered to stick to top, moved footer, nav and css import to sub template files

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,37 +1,19 @@
 package controllers
 
+import javax.inject.Inject
 import models.GetMovies
+import play.api.libs.ws._
 import play.api.mvc._
 
-
-import javax.inject.Inject
-import scala.concurrent.Future
-import scala.concurrent.duration._
-
-
-import play.api.libs.ws._
-import play.api.http.HttpEntity
-
-import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl._
-import akka.util.ByteString
-
-import scala.concurrent.ExecutionContext
 import scala.concurrent.ExecutionContext.Implicits.global
-import play.api.libs.json._
 
 class Application @Inject() (ws: WSClient) extends Controller {
 
 
   def showMovies = Action.async {
     ws.url("https://api.themoviedb.org/3/movie/now_playing?api_key=1c51d67c43ed71cbaa90f4a967f68650&language=en-US&page=1").get().map { response =>
-      Ok(views.html.home("Home Page")(response.body))
+      Ok(views.html.home("Home Page")(response.body)(views.html.footer()))
     }
-  }
-
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
   }
 
   def movieInfo(id: String) = Action.async {
@@ -41,6 +23,7 @@ class Application @Inject() (ws: WSClient) extends Controller {
   }
 
    def movieDetails(id: String) = Action {
-    Ok(views.html.movie(GetMovies.movieDetails(id)))
+    Ok(views.html.movie(GetMovies.movieDetails(id))(views.html.footer())
+    (views.html.headImports())(views.html.navigation()))
   }
 }
