0f69a6f6a3898aca497107736d8d1a315309a59a
seanmurray21071995 <sean.murray@academytrainee.com>
2018-11-01 17:13:35 +0000

    Done most off the search bar just need to fix the bug with the r var

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,17 +1,15 @@
 package controllers
-import models.GetMovies
+import models.{GetMovies, GetSearch}
 import play.api.mvc._
-
-
 import javax.inject.Inject
+
 import scala.concurrent.Future
 import scala.concurrent.duration._
-
-
 import play.api.libs.ws._
 import play.api.http.HttpEntity
-
 import akka.actor.ActorSystem
+import akka.actor.Status.Success
+import akka.japi.Option.Some
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl._
 import akka.util.ByteString
@@ -41,9 +39,13 @@ class Application @Inject() (ws: WSClient) extends Controller {
     Ok(views.html.movie(GetMovies.movieDetails(id)))
   }
 
-  def searchMovie(name: String)=Action.async{
+  def searchResult=Action(parse.form(GetSearch.createSearchForm)){request =>
+   val name:String = request.body.name
+    var r:String =""
     ws.url("https://api.themoviedb.org/3/movie/now_playing?api_key=1c51d67c43ed71cbaa90f4a967f68650&language=en-US&page=1").get().map{response =>
-      Ok(views.html.searchResults("result page")(response.body)(name))
+      r = response.body
     }
+    println("r is"+r+"name "+name)
+    Ok(views.html.searchResults("result page")(r)(name))
   }
 }
